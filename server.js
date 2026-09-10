4d61696b15fbfb94f56ff7dd8bf8e0a39e1b9522
adrid sadanand <adridsadanand@gmail.com>
2026-09-10 11:42:47 +0530

    port change from 5000 to 8080

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong on the server.' })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => app.listen(PORT, () => console.log(`Chitty Book API running on port ${PORT}`)))
